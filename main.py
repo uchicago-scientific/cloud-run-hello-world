c37ecc42a1d40db387f02395142c2bc012b1a0c8
tabinks <abinkowski@uchicago.edu>
2022-05-25 02:06:20 -0500

    Update emoji

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-import os, datetime, json
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
+from emoji import emojize
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def homepage():
-  return "Hello Photo Timeline!", 200
+    #https://emoji-translator-2022.uc.r.appspot.com/?name=:snake:&date=today
+    name = request.args.get('name', 'World')
+    return emojize(f"Hello {name}!!")
 
-if __name__ == '__main__':
-  app.run(debug=True)
+
+@app.route("/all", methods = ['POST', 'GET','DELETE'])
+def all():
+    #if request.method == 'POST':
+    emojis = dict()
+    with open('emojis.txt') as f:
+        lines = f.readlines()
+    for line in lines:
+        text = line.strip()
+        emojis[text] = emojize(text)
+    return json.dumps(emojis, indent=4, ensure_ascii=False)
+    jsonify json_data, 202
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
